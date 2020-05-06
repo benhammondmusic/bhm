bbf9dc43b4d930a4b1f79ee9d5eb9ad39decd5e2
BenHammondMusic <41567007+benhammondmusic@users.noreply.github.com>
2020-05-05 18:59:25 -0600

    added email php code, need to test on server

--- public_html/contact.php
+++ public_html/contact.php
@@ -1,1 +1,18 @@
-<?php phpinfo() ; ?>
+Thanks for your message!
+
+<?php
+	
+	$userName 		= $_POST['name'];
+	$userEmail	 	= $_POST['email'];
+	$userMessage 		= $_POST['message'];
+
+	$to 			= "me@benhammondmusic.com";
+	$subject 		= "Website Contact Form";
+	$body 			= "Information Submitted:";
+
+	$body .= "\r\n Name: " . $userName;
+    $body .= "\r\n Email: " . $userEmail; 	
+	$body .= "\r\n Message: " . $userMessage;
+
+	mail($to, $subject, $body);
+?>
